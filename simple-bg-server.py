aec0a4ae28581516ac61031d1e50616bcd26728a
nightmar39 <anthonyzilla94@gmail.com>
2022-10-05 16:51:52 -0700

    adding initial files

---
+++ simple-bg-server.py
@@ -0,0 +1,35 @@
+import json, os   
+from flask import Flask
+from queue import Queue
+
+
+app = Flask(__name__)
+hits = []
+hits_q = Queue(maxsize = 6 )
+version = os.environ.get('VERSION')
+
+def append_version(curr_version): 
+    if hits_q.full(): 
+        hits_q.get()
+
+    hits_q.put(curr_version)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/version")
+def return_version():
+    append_version(version)
+    return f"<p>{version}<p>"
+
+@app.route("/hits")
+def print_hits():
+    hits = list(hits_q.queue)
+    jsonStr= json.dumps(hits)
+    return jsonStr
+
+if __name__ == '__main__':
+    app.run(debug=True, port='8000')
+
