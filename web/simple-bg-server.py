7555efadc1be8c8928361d64043b7cb805a70fd7
Anthony Rozario <59712821+nightmar39@users.noreply.github.com>
2022-10-18 12:13:25 -0700

    Update simple-bg-server.py

--- web/simple-bg-server.py
+++ web/simple-bg-server.py
@@ -20,7 +20,7 @@ def hello_world():
 
 @app.route("/demo")
 def hello_world():
-    return "<h1>Hello, DEMO cx2</h1>"
+    return "<h1>Hello, DEMO cx3</h1>"
 
 @app.route("/version")
 def return_version():
